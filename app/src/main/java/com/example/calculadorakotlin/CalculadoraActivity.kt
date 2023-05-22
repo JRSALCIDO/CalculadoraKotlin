db7ed67cdc8588920206c6ce49851aed3b1f2dae
JRSALCIDO <salcido.12311@gmail.com>
2023-05-22 13:44:27 -0700

    Version Final

--- app/src/main/java/com/example/calculadorakotlin/CalculadoraActivity.kt
+++ app/src/main/java/com/example/calculadorakotlin/CalculadoraActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class CalculadoraActivity : AppCompatActivity() {
@@ -37,31 +38,68 @@ class CalculadoraActivity : AppCompatActivity() {
         btnSalir = findViewById(R.id.btnSalir)
 
         btnSuma.setOnClickListener {
-            calculadora = Calculadora(txtNum1.text.toString().toFloat(), txtNum2.text.toString().toFloat())
-            val resultado = calculadora?.suma()
-            lblResultado.text = resultado.toString()
+            val num1 = txtNum1.text.toString().trim()
+            val num2 = txtNum2.text.toString().trim()
+
+            if (num1.isNotEmpty() && num2.isNotEmpty()) {
+                // Ambos campos contienen valores no vacíos
+                calculadora = Calculadora(num1.toFloat(), num2.toFloat())
+                val resultado = calculadora?.suma()
+                lblResultado.text = resultado.toString()
+            }  else {
+                Toast.makeText(this, "Ingrese Ambos Numeros", Toast.LENGTH_SHORT).show()
+            }
+
+
         }
 
         btnResta.setOnClickListener {
-            calculadora = Calculadora(txtNum1.text.toString().toFloat(), txtNum2.text.toString().toFloat())
-            val resultado = calculadora?.resta()
-            lblResultado.text = resultado.toString()
+            val num1 = txtNum1.text.toString().trim()
+            val num2 = txtNum2.text.toString().trim()
+
+            if (num1.isNotEmpty() && num2.isNotEmpty()) {
+                // Ambos campos contienen valores no vacíos
+                calculadora = Calculadora(txtNum1.text.toString().toFloat(), txtNum2.text.toString().toFloat())
+                val resultado = calculadora?.resta()
+                lblResultado.text = resultado.toString()
+            }  else {
+                Toast.makeText(this, "Ingrese Ambos Numeros", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         btnMult.setOnClickListener {
-            calculadora = Calculadora(txtNum1.text.toString().toFloat(), txtNum2.text.toString().toFloat())
-            val resultado = calculadora?.multiplicacion()
-            lblResultado.text = resultado.toString()
+            val num1 = txtNum1.text.toString().trim()
+            val num2 = txtNum2.text.toString().trim()
+
+            if (num1.isNotEmpty() && num2.isNotEmpty()) {
+                // Ambos campos contienen valores no vacíos
+                calculadora = Calculadora(txtNum1.text.toString().toFloat(), txtNum2.text.toString().toFloat())
+                val resultado = calculadora?.multiplicacion()
+                lblResultado.text = resultado.toString()
+            }  else {
+                Toast.makeText(this, "Ingrese Ambos Numeros", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         btnDiv.setOnClickListener {
-            calculadora = Calculadora(txtNum1.text.toString().toFloat(), txtNum2.text.toString().toFloat())
-            val resultado = calculadora?.division()
-            if (resultado != null) {
-                lblResultado.text = resultado.toString()
-            } else {
-                lblResultado.text = "Error: División por cero"
+            val num1 = txtNum1.text.toString().trim()
+            val num2 = txtNum2.text.toString().trim()
+
+            if (num1.isNotEmpty() && num2.isNotEmpty()) {
+                // Ambos campos contienen valores no vacíos
+                calculadora = Calculadora(txtNum1.text.toString().toFloat(), txtNum2.text.toString().toFloat())
+                val resultado = calculadora?.division()
+                if (resultado != null) {
+                    lblResultado.text = resultado.toString()
+                } else {
+                    lblResultado.text = "Error: División por cero"
+                }
+            }  else {
+                Toast.makeText(this, "Ingrese Ambos Numeros", Toast.LENGTH_SHORT).show()
             }
+
         }
 
         btnLimpiar.setOnClickListener {
